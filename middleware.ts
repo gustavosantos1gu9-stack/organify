2b25506baa20359cfaddeee133eaf6851ee9f028
Gustavo Guilherme <gustavoguilherme@MacBook-Air-de-Gustavo.local>
2026-03-16 17:21:31 -0300

    fix: ativar login

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,15 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export async function middleware(req: NextRequest) {
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("sb-dfjmnibyfowwmlmfbbpa-auth-token");
+  const isAuth = !!token;
+  const isLoginPage = req.nextUrl.pathname === "/login";
+  const isPublic = req.nextUrl.pathname.startsWith("/c") || req.nextUrl.pathname.startsWith("/api");
+
+  if (isPublic) return NextResponse.next();
+  if (!isAuth && !isLoginPage) return NextResponse.redirect(new URL("/login", req.url));
+  if (isAuth && isLoginPage) return NextResponse.redirect(new URL("/", req.url));
   return NextResponse.next();
 }
 
