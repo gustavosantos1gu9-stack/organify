b91b2ac7dbf175ea5acba24843bf5498f4914950
Gustavo Guilherme <gustavoguilherme@MacBook-Air-de-Gustavo.local>
2026-03-16 23:05:47 -0300

    feat: ativar login

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,19 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
+  const token = req.cookies.get("sb-dfjmnibyfowwmlmfbbpa-auth-token");
+  const isAuth = !!token;
+  const path = req.nextUrl.pathname;
+
+  const isPublic =
+    path === "/login" ||
+    path === "/redefinir-senha" ||
+    path === "/nova-senha" ||
+    path.startsWith("/c") ||
+    path.startsWith("/api");
+
+  if (isPublic) return NextResponse.next();
+  if (!isAuth) return NextResponse.redirect(new URL("/login", req.url));
   return NextResponse.next();
 }
 
